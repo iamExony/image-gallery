a87a8d3747e60a084a6fe4b286dea11236dd0120
onye4holiness@gmail.com <melody2omoro@gmail.com>
2023-09-23 14:21:29 +0100

    updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -41,20 +41,20 @@ function LoginForm() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div className="min-h-screen  flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("/background.jfif")'}} >
+      <div className="max-w-md w-full space-y-8 backdrop-blur-md bg-white/30 px-8 py-12 rounded-xl">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">{login ? (<>
-          <h2>Welcome to Gallery</h2>
+          <h2 className='text-black'>Welcome to Gallery</h2>
           </>) : (<>
-          <h2>Welcome to Gallery</h2>
-          <span className='text-sm'>Get Awsome Photo Gallery</span>
+          <h2 className='text-black'>Welcome to Gallery</h2>
+          <span className='text-sm text-black'>Get Awsome Photo Gallery</span>
           </>)}</h2>
         </div>
         <form className="mt-8 space-y-6" onSubmit={(e) => handleSubmit(e, login ? 'signin' : 'signup')}>
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
-              <label htmlFor="email" className="sr-only">
+              <label htmlFor="email" className="text-black font-bold">
                 Email address
               </label>
               <input
@@ -65,12 +65,12 @@ function LoginForm() {
                 required
                 value={email}
                 onChange={handleEmailChange}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border rounded-t-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none relative block w-full px-3 py-2 border rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-transparent focus:z-10 sm:text-sm"
                 placeholder="Email address"
               />
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">
+              <label htmlFor="password" className=" text-black font-bold">
                 Password
               </label>
               <input
@@ -81,7 +81,7 @@ function LoginForm() {
                 required
                 value={password}
                 onChange={handlePasswordChange}
-                className="appearance-none rounded-none relative block w-full px-3 py-2 border rounded-b-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                className="appearance-none relative block w-full px-3 py-2 border rounded-md placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-transparent focus:z-10 sm:text-sm"
                 placeholder="Password"
               />
             </div>
@@ -101,7 +101,7 @@ function LoginForm() {
             </div>
 
             <div className="text-sm">
-              <a href="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+              <a href="/" className="font-medium text-black hover:text-red-900">
                 Forgot your password?
               </a>
             </div>
@@ -110,14 +110,14 @@ function LoginForm() {
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black hover:bg-red-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               {login ? "Sign In" : "Sign Up"}
             </button>
           </div>
-          <div className='flex items-center justify-center'>
-            <p className={login ?'hidden' : ''}>Already a Member? <span onClick={() => setLogin(true)} role='button'>Sign In</span></p>
-            <p className={login ?'' : 'hidden'}>New User? <span onClick={() => setLogin(false)} role='button'>Sign Up</span></p>
+          <div className='flex items-center justify-center text-black text-xl font-semibold'>
+            <p className={login ?'hidden' : ''}>Already a Member? <span onClick={() => setLogin(true)} role='button' className='hover:text-red-900'>Sign In</span></p>
+            <p className={login ?'' : 'hidden'}>New User? <span onClick={() => setLogin(false)} role='button' className='hover:text-red-900'>Sign Up</span></p>
           </div>
         </form>
       </div>
